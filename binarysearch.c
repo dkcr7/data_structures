4f6d5149ea9ccd779e47fa7363d7086629a9bca8
bansaldhruv3012 <bansaldhruv3012@gmail.com>
2019-07-13 00:03:11 +0530

    code

---
+++ binarysearch.c
@@ -0,0 +1,56 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code
+                , Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int binarysearch(int arr[],int srt,int end,int ser)
+{
+       int mid=srt+(end-1)/2;
+       if(srt==end&&arr[mid]!=ser)
+       {
+           return -1;
+       }
+       if(arr[mid]==ser)
+       {
+           return mid;
+       }
+       else if(arr[mid]>ser)
+       {
+           return binarysearch(arr,srt,mid-1,ser);
+       }
+       else
+       {
+           return binarysearch(arr,mid+1,end,ser);
+       }
+}
+int main()
+{
+    int n;
+    printf("Enter size of array:");
+    scanf("%d",&n);
+    int arr[100];
+    printf("Enter elements in array:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int sr;
+    printf("Enter element to be searched:");
+    scanf("%d",&sr);
+    int res=binarysearch(arr,0,n-1,sr);
+    if(res==-1)
+    {
+        printf("Not Found");
+    }
+    else
+    {
+        printf("index: %d",res);
+    }
+    return 0;
+}
+
